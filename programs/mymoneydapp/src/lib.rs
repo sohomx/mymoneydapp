8a3bd17d5545087152b1625c1d2bc94d83791564
sohom <sohom377@gmail.com>
2022-02-03 20:24:55 +0530

    logic for the first command

--- programs/mymoneydapp/src/lib.rs
+++ programs/mymoneydapp/src/lib.rs
@@ -7,15 +7,15 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod mymoneydapp {
     use super::*;
     pub fn proxy_transfer(ctx: Context<ProxyTransfer>, amount: u64) -> ProgramResult {
-
+        token::transfer(ctx.accounts.info(), amount)
     }
 
     pub fn proxy_mint_to(ctx: Context<ProxyMintTo>, amount: u64) -> ProgramResult {
-
+        token::mint_to(ctx.accounts.into(), amount)
     }
 
     pub fn proxy_burn(ctx: Context<ProxyBurn>, amount: u64) -> ProgramResult {
-
+        token::burn(ctx.accounts.into(), amount)
     }
 
     pub fn proxy_set_authority(
@@ -23,7 +23,7 @@ pub mod mymoneydapp {
         authority_type: AuthorityType,
         new_authority: Option<Pubkey>,
     ) -> ProgramResult {
-        
+        token::burn(ctx.accounts.into(), authority_type.into(), new_authority)
     }
 }
  
