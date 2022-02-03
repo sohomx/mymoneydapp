37f5f292ee83e3c6db97ca2b6e8ba7a62a0e5916
sohom <sohom377@gmail.com>
2022-02-03 20:14:57 +0530

    wrote the first program function

--- programs/mymoneydapp/src/lib.rs
+++ programs/mymoneydapp/src/lib.rs
@@ -6,10 +6,26 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod mymoneydapp {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
+    pub fn proxy_transfer(ctx: Context<ProxyTransfer>, amount: u64) -> ProgramResult {
+
+    }
+
+    pub fn proxy_mint_to(ctx: Context<ProxyMintTo>, amount: u64) -> ProgramResult {
+
+    }
+
+    pub fn proxy_burn(ctx: Context<ProxyBurn>, amount: u64) -> ProgramResult {
+
+    }
+
+    pub fn proxy_set_authority(
+        ctx: Context<ProxySetAuthority>,
+        authority_type: AuthorityType,
+        new_authority: Option<Pubkey>,
+    ) -> ProgramResult {
+        
     }
 }
-
+ 
 #[derive(Accounts)]
 pub struct Initialize {}
