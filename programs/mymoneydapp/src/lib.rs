59b0b43fa380a2d376f4ea6efddc4339fa6eab77
sohom <sohom377@gmail.com>
2022-02-04 19:47:19 +0530

    Testing the transfer of authority

--- programs/mymoneydapp/src/lib.rs
+++ programs/mymoneydapp/src/lib.rs
@@ -6,33 +6,34 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod mymoneydapp {
     use super::*;
-    pub fn proxy_transfer(ctx: Context<ProxyTransfer>, amount: u64) -> ProgramResult {
-        token::transfer(ctx.accounts.info(), amount)
-    }
 
+    pub fn proxy_transfer(ctx: Context<ProxyTransfer>, amount: u64) -> ProgramResult {
+        token::transfer(ctx.accounts.into(), amount)
+    }
+    
     pub fn proxy_mint_to(ctx: Context<ProxyMintTo>, amount: u64) -> ProgramResult {
         token::mint_to(ctx.accounts.into(), amount)
     }
-
+    
     pub fn proxy_burn(ctx: Context<ProxyBurn>, amount: u64) -> ProgramResult {
         token::burn(ctx.accounts.into(), amount)
     }
-
+    
     pub fn proxy_set_authority(
         ctx: Context<ProxySetAuthority>,
         authority_type: AuthorityType,
         new_authority: Option<Pubkey>,
     ) -> ProgramResult {
-        token::burn(ctx.accounts.into(), authority_type.into(), new_authority)
+        token::set_authority(ctx.accounts.into(), authority_type.into(), new_authority)
     }
 }
- 
+
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub enum AuthorityType {
     MintTokens,     // Authority to mint new tokens
-    FreezeAccount,  // Authority to freeze any account associated with the mint
-    AccountOwner,   // Owner of a given token account
-    CloseAccount,   // Authority to close a token Account
+    FreezeAccount, // Authority to freeze any account associated with the Mint
+    AccountOwner, // Owner of a given token account
+    CloseAccount, // Authority to close a token account
 }
 
 #[derive(Accounts)]
@@ -40,10 +41,10 @@ pub struct ProxyTransfer<'info> {
     #[account(signer)]
     pub authority: AccountInfo<'info>,
     #[account(mut)]
-    pub from: AccountInfo<'info>,
+    pub from:AccountInfo<'info>,
     #[account(mut)]
-    pub to:AccountInfo<'info>,
-    pub_token_program: AccountInfo<'info>,
+    pub to: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
@@ -77,12 +78,12 @@ pub struct ProxySetAuthority<'info> {
     pub token_program: AccountInfo<'info>,
 }
 
-impl <'a, 'b, 'c, 'info> From<&mut ProxyTransfer<'info>> 
+impl<'a, 'b, 'c, 'info> From<&mut ProxyTransfer<'info>>
     for CpiContext<'a, 'b, 'c, 'info, Transfer<'info>>
 {
     fn from(accounts: &mut ProxyTransfer<'info>) -> CpiContext<'a, 'b, 'c, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
-            from: accounts.to.clone(),
+            from: accounts.from.clone(),
             to: accounts.to.clone(),
             authority: accounts.authority.clone(),
         };
@@ -105,11 +106,9 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxyMintTo<'info>>
     }
 }
 
-impl<'a, 'b, 'c, 'info> From<&mut ProxyBurn<'info>> 
-    for CpiContext<'a, 'b, 'c, 'info, Burn<'info>> 
-{
-    fn from(accounts: &mut ProxyBurn<'info>) -> CpiContext<'a, 'b, 'c, 'info, MintTo<'info>> {
-        let cpi_accounts = MintTo {
+impl<'a, 'b, 'c, 'info> From<&mut ProxyBurn<'info>> for CpiContext<'a, 'b, 'c, 'info, Burn<'info>> {
+    fn from(accounts: &mut ProxyBurn<'info>) -> CpiContext<'a, 'b, 'c, 'info, Burn<'info>> {
+        let cpi_accounts = Burn {
             mint: accounts.mint.clone(),
             to: accounts.to.clone(),
             authority: accounts.authority.clone(),
@@ -134,14 +133,13 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxySetAuthority<'info>>
     }
 }
 
-impl From<AuthorityType> for spl_token::instruction::AuthorityType{
-    fn from(authority_ty:AuthorityType) -> spl_token::instruction::AuthorityType{
-        match authority_ty{
+impl From<AuthorityType> for spl_token::instruction::AuthorityType {
+    fn from(authority_ty: AuthorityType) -> spl_token::instruction::AuthorityType {
+        match authority_ty {
             AuthorityType::MintTokens => spl_token::instruction::AuthorityType::MintTokens,
             AuthorityType::FreezeAccount => spl_token::instruction::AuthorityType::FreezeAccount,
             AuthorityType::AccountOwner => spl_token::instruction::AuthorityType::AccountOwner,
-            AuthorityType::CloseAccount => 
-            spl_token::instruction::AuthorityType::CloseAccount,
+            AuthorityType::CloseAccount => spl_token::instruction::AuthorityType::CloseAccount,
         }
     }
 }
